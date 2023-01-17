b91fdf59a9cfaa6b00a073943a86f16be87b37aa
cc211002 <cc211002@fhstp.ac.at>
2023-01-17 09:54:29 +0100

    bottom bar shit yo

--- app/src/main/java/com/example/todolist_app_project/MainActivity.kt
+++ app/src/main/java/com/example/todolist_app_project/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,22 +30,25 @@ class MainActivity : ComponentActivity() {
                                 contentDescription = "Menu",
                             )
                             Spacer(modifier = Modifier.width(200.dp))
-                            Button(onClick={/* */},
-                            contentPadding = PaddingValues(
-                                start= 20.dp
-                            )) {
+                            Button(
+                                onClick = {/* */ },
+                                contentPadding = PaddingValues(
+                                    start = 20.dp
+                                )
+                            ) {
                                 Text("Favorites")
                             }
                             Spacer(modifier = Modifier.width(30.dp))
-                            Button(onClick={},
-                            contentPadding= PaddingValues(
-                                start=10.dp,end=10.dp
-                            )
+                            Button(
+                                onClick = {},
+                                contentPadding = PaddingValues(
+                                    start = 10.dp, end = 10.dp
+                                )
                             ) {
                                 Icon(
                                     Icons.Filled.Person,
-                                    contentDescription="Settings",
-                                    modifier=Modifier.size(ButtonDefaults.IconSize)
+                                    contentDescription = "Settings",
+                                    modifier = Modifier.size(ButtonDefaults.IconSize)
                                 )
                             }
                         }
@@ -56,7 +56,25 @@ class MainActivity : ComponentActivity() {
                 ) {
                     // Screen content
                 }
+
+//               bottom bar init
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar {
+                            Icon(
+                                Icons.Filled.Edit,
+                                contentDescription = "create the weekly list",
+                                modifier=Modifier.size(30.dp)
+                            )
+                        }
+                    }
+                ) {
+                    // Screen content
+                }
+
             }
+
+
         }
     }
 }
