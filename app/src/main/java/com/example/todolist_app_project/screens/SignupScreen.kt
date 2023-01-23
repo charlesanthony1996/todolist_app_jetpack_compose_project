a1bf52ad179e0f3c311207ccd530b80b6c8c2b2d
cc211002 <cc211002@fhstp.ac.at>
2023-01-23 10:55:58 +0100

    latest

--- app/src/main/java/com/example/todolist_app_project/screens/SignupScreen.kt
+++ app/src/main/java/com/example/todolist_app_project/screens/SignupScreen.kt
@@ -1,2 +1,14 @@
 package com.example.todolist_app_project.screens
 
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import com.example.todolist_app_project.LoginViewModel
+
+
+@Composable
+fun SignupScreen(loginviewModel: LoginViewModel) {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+}
