adaf31b009f43048ef54079624fe2bcf6d5afb10
cc211002 <cc211002@fhstp.ac.at>
2023-01-24 21:24:09 +0100

    minor changes

--- app/src/main/java/com/example/todolist_app_project/screens/ManualEntryScreen.kt
+++ app/src/main/java/com/example/todolist_app_project/screens/ManualEntryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.todolist_app_project.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.todolist_app_project.LoginViewModel
@@ -25,8 +27,37 @@ fun CreateManualEntryScreen(viewModel: LoginViewModel) {
             .verticalScroll(rememberScrollState())
             .padding(start = 24.dp, top = 24.dp, end = 24.dp, bottom = 24.dp)
             .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-        Text("Create manual entry screen")
+        if (viewModel.error.value.isNotBlank()) {
+            ErrorField(viewModel)
+        }
+
+//        render composable functions here
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
