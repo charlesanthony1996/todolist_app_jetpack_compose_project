88b84f4de40d8120ddc290b108878a3555c87ff7
cc211002 <cc211002@fhstp.ac.at>
2023-01-24 18:21:46 +0100

    sign up changes

--- app/src/main/java/com/example/todolist_app_project/screens/SignupScreen.kt
+++ app/src/main/java/com/example/todolist_app_project/screens/SignupScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.todolist_app_project.LoginViewModel
 import com.example.todolist_app_project.R
@@ -37,6 +38,7 @@ fun SignupScreen(viewModel: LoginViewModel) {
 //        add composable functions here
         NameFieldElement(viewModel)
         EmailFieldElement(viewModel)
+        PasswordFieldElement(viewModel)
         CreateSignupButton(viewModel)
 
 
@@ -72,13 +74,27 @@ fun EmailFieldElement(viewModel: LoginViewModel) {
 }
 
 @Composable
+fun PasswordFieldElement(viewModel: LoginViewModel) {
+    val password = viewModel.password.value
+
+    Text("Password")
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        visualTransformation = PasswordVisualTransformation(),
+        value = password,
+        label = { Text(text = stringResource(R.string.password)) },
+        onValueChange = { viewModel.setPassword(it) }
+    )
+}
+
+@Composable
 fun CreateSignupButton(viewModel: LoginViewModel) {
     Button(
         modifier = Modifier
             .fillMaxWidth()
             .height(50.dp),
         enabled = viewModel.isValidEmailAndPassword(),
-        content = { Text(text = stringResource(R.string.create)) },
+        content = { Text(text = stringResource(R.string.create_account)) },
         onClick = { viewModel.createUserWithEmailAndPassword() }
     )
 }
